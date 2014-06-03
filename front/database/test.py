30ae4ad36d26f7c912056441ae3362a8c55a8efe
Sujit Shakya <sshakya.mail@gmail.com>
2014-06-03 18:02:20 +0545

    test continuee....

--- front/database/test.py
+++ front/database/test.py
@@ -9,7 +9,22 @@ def main():
         sys.stderr.write("Connection Successfull")
     except ConnectionFailure as e:
         sys.stderr.write("connection unsuccesfull: %s" % e)
-        # sys.exit(1)
+        sys.exit(1)
+
+    # Get the database handle to the database named "testDb"
+    dbh = c["testDb"]
+    assert dbh.connection == c
+
+    # insert document into collection
+    user_doc = {
+        "username": "sujit",
+        "address": "Ktm",
+        "gender": "mail"
+    }
+    dbh.users.insert(user_doc, safe = True)
+    print("user_doc document has been added in the users collection")
+
+
 
 if __name__ == '__main__':
     main()
